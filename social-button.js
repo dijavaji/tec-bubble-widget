a170a5f309837d52ac57568a011231ce0881772b
diegomac <dijavaji@gmail.com>
2026-03-24 18:31:02 -0500

    cambio iconos

--- social-button.js
+++ social-button.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
   socialBtn.classList.add('btn', 'btn-primary', 'shadow-lg', 'justify-content-center', 'align-items-center', 'social-btn');
 
   const chatWidgetButtonIcon = document.createElement('img');
-  chatWidgetButtonIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/icon_chatbot.svg';
+  chatWidgetButtonIcon.src = 'public/assets/img/icon_message.svg';
   chatWidgetButtonIcon.alt = 'chat';
   chatWidgetButtonIcon.id = 'chatWidgetButtonIcon';
   chatWidgetButtonIcon.style.width = '51.82px';
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
   chatHeader.classList.add('chat-header');
 
   const chatHeaderIcon = document.createElement('img');
-  chatHeaderIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/icon_chatbot.svg';
+  chatHeaderIcon.src = 'public/assets/img/icon_message.svg';
   chatHeaderIcon.alt = 'chat';
   chatHeaderIcon.id = 'chatWidgetButtonIcon';
   chatHeaderIcon.style.width = '33.12px';
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function() {
   sendButton.type = 'submit';
 
   const sendButtonIcon = document.createElement('img');
-  sendButtonIcon.src = 'https://raw.githubusercontent.com/Praxis-Innovacion/tec-bubble-embed/bffa0d745e7e6f70982cda04cf8b85700b3a43be/public/assets/img/send10.svg';
+  sendButtonIcon.src = 'public/assets/img/icon_send.svg';
   sendButtonIcon.alt = 'Ejecutar';
   sendButtonIcon.id = 'executeBtn';
   sendButtonIcon.onclick = ejecutarAccion; //
